05938bf0085dbfaabe4826f5a4023e9212e13f7f
Stanislav Kozubenko <staspk@gmail.com>
2025-03-27 16:13:16 -0700

    removed redundant function

--- utils.py
+++ utils.py
@@ -3,6 +3,3 @@ import random, string
 class Utils:
     def get_randomized_string(str_length):
         return ''.join(random.choices(string.ascii_letters, k=str_length))
-    
-    def random_int(x, y):
-        return random.randint(x, y)
