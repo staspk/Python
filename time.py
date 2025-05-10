7c2e2b98a3b14daf3fb6500579d383331ddb80f2
Stanislav Kozubenko <staspk@gmail.com>
2025-05-10 08:22:22 -0700

    +local_time_as_legal_filename()

--- time.py
+++ time.py
@@ -31,3 +31,9 @@ class Time:
         returns local_time in format: `2025-03-18 16:30:55`
         """
         return datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+    
+    def local_time_as_legal_filename() -> str:
+        """
+        returns local_time in format that won't throw exception if used as filename: `2025-03-18 16:30:55`
+        """
+        return datetime.now().strftime('%Y-%m-%d_%H.%M.%S')
