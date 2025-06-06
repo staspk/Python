a2d97e6475ebc593b3afaf211132bc0015fc95b6
Stanislav Kozubenko <staspk@gmail.com>
2025-06-06 16:04:06 -0700

    Timer measurements now console-print in yellow

--- time.py
+++ time.py
@@ -1,6 +1,8 @@
 from datetime import datetime
 import time
 
+from kozubenko.print import print_yellow
+
 class Timer:
     start_time: float = None
 
@@ -11,17 +13,17 @@ class Timer:
         elapsed = (time.perf_counter() - Timer.start_time) * 1000                          #   converts elapsed from ns to ms
         if elapsed > 1000:                                                                 #   if elapsed more than 1 second:
             elapsed = elapsed / 1000                                                       #
-            print(f'{timed_operation_title} Operation timed at: {elapsed:.3f}s')           #       convert and print in seconds, like: 1.234s 
+            print_yellow(f'{timed_operation_title} Operation timed at: {elapsed:.3f}s')           #       convert and print in seconds, like: 1.234s 
         else:                                                                              #   else:
-            print(f'{timed_operation_title} Operation timed at: {elapsed:.3f}ms')          #        print in ms, like:                 123.456ms or 0.000ms under 500ns
+            print_yellow(f'{timed_operation_title} Operation timed at: {elapsed:.3f}ms')          #        print in ms, like:                 123.456ms or 0.000ms under 500ns
 
     def stop():
         elapsed = (time.perf_counter() - Timer.start_time) * 1000
         if elapsed > 1000:
             elapsed = elapsed / 1000
-            print(f'Operation timed at: {elapsed:.3f}s')
+            print_yellow(f'Operation timed at: {elapsed:.3f}s')
         else:
-            print(f'Operation timed at: {elapsed:.3f}ms')
+            print_yellow(f'Operation timed at: {elapsed:.3f}ms')
 
         Timer.start_time = None
 
