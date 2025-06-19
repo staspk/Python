26147b925dda43d1884c8d1e9f7c5895a9454848
Stanislav Kozubenko <staspk@gmail.com>
2025-06-18 20:07:25 -0700

    assert_list() : +returnBool (opt param)

--- utils.py
+++ utils.py
@@ -29,19 +29,30 @@ def assert_int(var_name:str, _int:int, min_val:int=None, max_val:int=None):
     if(max_val) and _int > max_val:
         raise Exception(f"AssertInt({var_name}): value of int > max_val: {max_val}")
 
-def assert_list(var_name:str, _list:list, min_len:int=None, max_len:int=None):
+def assert_list(var_name:str, _list:list, min_len:int=None, max_len:int=None, returnBool=False):
     """
-    Use to enforce type annotations. Will raise Exception:
+    Use to enforce type annotations. Raises Exception, unless `returnBool=True`:
     * if _list is not a list
     * len(_list) < min_len (*optional*)
     * len(_list) > max_len (*optional*)
+    * returnBool (*optional*)
     """
-    if not type(_list) is list:
-        raise Exception(f"AssertList({var_name}): must be a list, not: {type(_list)}")
-    if(min_len) and len(_list) < min_len:
-        raise Exception(f"AssertList({var_name}): length of list under min_len: {min_len}")
-    if(max_len) and len(_list) > max_len:
-        raise Exception(f"AssertList({var_name}): length of list exceeds max_len: {max_len}")
+    if not returnBool:    
+        if not isinstance(_list) is list:
+            raise Exception(f"AssertList({var_name}): must be a list, not: {type(_list)}")
+        if(min_len) and len(_list) < min_len:
+            raise Exception(f"AssertList({var_name}): length of list under min_len: {min_len}")
+        if(max_len) and len(_list) > max_len:
+            raise Exception(f"AssertList({var_name}): length of list exceeds max_len: {max_len}")
+    else:
+        if not isinstance(_list, list):
+            return False
+        if min_len is not None and len(_list) < min_len:
+            return False
+        if max_len is not None and len(_list) > max_len:
+            return False
+        return True
+
 
 def assert_path_exists(var_name:str, path:str):
     """
