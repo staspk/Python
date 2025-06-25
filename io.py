43bf9a2fd8d49b18bfafd0ac5e93b6ee53383520
Stanislav Kozubenko <staspk@gmail.com>
2025-06-24 22:02:12 -0700

    load_json() opt capability to convert to desired class

--- io.py
+++ io.py
@@ -2,17 +2,24 @@ import json, re
 from kozubenko.print import print_red
 
 
-def load_json(file:str) -> any:
+def load_json(file:str, class_name=None) -> any:
     """
-    `print(json.dumps(data, indent=4))`
+    Usage: `songs = load_json("restricted_songs.json", ISong)`
+
+    class_name currently only works with 1-dimensional 
+
+    To print after: `print(json.dumps(data, indent=4))`
     """
     try:
         with open(file, 'r', encoding='utf-8') as file:
             data = json.load(file)
-        return data
+        if class_name is None:
+            return data
     except Exception as e:
-        print_red(f'Error in kozubenko.io.load_json(file:str):\n{e}')
+        print_red(f"Error in kozubenko.io.load_json(file:str):\n{e}")
         raise e
+    
+    return [class_name(**item) for item in data]
     
 def load_file(file:str) -> str:
     try:
