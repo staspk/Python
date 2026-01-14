7dfd192d00e1fdd57c6a248c9968ae8db4d67ee0
Stanislav Kozubenko <staspk@gmail.com>
2026-01-14 00:12:26 -0800

    +class_attributes()

--- cls.py
+++ cls.py
@@ -2,8 +2,12 @@ from types import SimpleNamespace
 from typing import Any, Generator
 
 
-def class_attributes():
-    pass
+def class_attributes(cls) -> Generator[Any]:
+    for key,value in cls.__dict__.items():
+        if key.startswith('__'): continue
+        if callable(value): continue
+        if isinstance(value, (classmethod, property)): continue
+        yield key,value
 
 def instance_attributes(instance, keys_to_exclude:list=[]) -> Generator[Any]:
     """
@@ -18,3 +22,13 @@ def instance_attributes(instance, keys_to_exclude:list=[]) -> Generator[Any]:
     for key,value in instance.__dict__.items():
         if key in keys_to_exclude: continue
         yield SimpleNamespace(key=key, value=value)
+
+def instance_attribute_keys(instance, keys_to_exclude:list=[]) -> Generator[Any]:
+    for key in instance.__dict__.keys():
+        if key in keys_to_exclude: continue
+        yield key
+
+def instance_attribute_values(instance, keys_to_exclude:list=[]) -> Generator[Any]:
+    for key,value in instance.__dict__.items():
+        if key in keys_to_exclude: continue
+        yield value
