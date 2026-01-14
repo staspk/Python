1402d788ad4971e953bfbd9796646bbc45b49334
Stanislav Kozubenko <staspk@gmail.com>
2026-01-14 00:12:59 -0800

    +random_pop(), +next_random_pop()

--- random.py
+++ random.py
@@ -1,4 +1,5 @@
 import string, random
+from typing import Any, Generator
 
 
 class Random():
@@ -7,3 +8,15 @@ class Random():
         returns a randomized string
         """
         return ''.join(random.choices(of, k=len))
+    
+def next_random_pop(set:set) -> Generator[Any]:
+    while set.__len__() > 0:
+        value = random.choice(tuple(set))
+        set.remove(value)
+        yield value
+
+def random_pop(set:set) -> Any:
+    if set.__len__() > 0:
+        value = random.choice(tuple(set))
+        set.remove(value)
+        return value
