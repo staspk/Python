c323a4eed20937778fc60ff3207a9ef10c76c949
Stanislav Kozubenko <staspk@gmail.com>
2026-01-21 12:12:47 -0800

    Return Value Fix

--- cls.py
+++ cls.py
@@ -1,34 +1,13 @@
-from types import SimpleNamespace
-from typing import Any, Generator
+from typing import Any, Iterator
 
 
-def class_attributes(cls) -> Generator[Any]:
+def class_attributes(cls) -> Iterator[Any]:
     for key,value in cls.__dict__.items():
         if key.startswith('__'): continue
         if callable(value): continue
         if isinstance(value, (classmethod, property)): continue
         yield key,value
 
-def instance_attributes(instance, keys_to_exclude:list=[]) -> Generator[Any]:
-    """
-    **How to Use:**
-    ```python
-    for attr in instance_attributes(cls_instance, keys_to_exclude=['special_key']):
-        translation:str = attr.key
-        marked_chapters:set[int] = attr.value
-        Print.green(f'{translation} -> {str(marked_chapters)}')
-    ```
-    """
-    for key,value in instance.__dict__.items():
-        if key in keys_to_exclude: continue
-        yield SimpleNamespace(key=key, value=value)
-
-def instance_attribute_keys(instance, keys_to_exclude:list=[]) -> Generator[Any]:
-    for key in instance.__dict__.keys():
-        if key in keys_to_exclude: continue
-        yield key
-
-def instance_attribute_values(instance, keys_to_exclude:list=[]) -> Generator[Any]:
-    for key,value in instance.__dict__.items():
-        if key in keys_to_exclude: continue
-        yield value
+def set_frozen_attr(instance, attr_name:str, value):
+    """ bypasses: `@dataclass(frozen=True)` """
+    object.__setattr__(instance, attr_name, value)
--- random.py
+++ random.py
@@ -1,5 +1,5 @@
 import string, random
-from typing import Any, Generator
+from typing import Any, Iterator
 
 
 class Random():
@@ -9,7 +9,7 @@ class Random():
         """
         return ''.join(random.choices(of, k=len))
     
-def next_random_pop(set:set) -> Generator[Any]:
+def next_random_pop(set:set) -> Iterator[Any]:
     while set.__len__() > 0:
         value = random.choice(tuple(set))
         set.remove(value)
