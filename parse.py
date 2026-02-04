8e7f9eaba2ef6b65454c5ec5a5f1d41c66aa9dae
Stanislav Kozubenko <staspk@gmail.com>
2026-02-03 22:49:14 -0800

    +is_AlphaNumeric()

--- parse.py
+++ parse.py
@@ -4,3 +4,7 @@ def substring_between(string:str, char1:str|int, char2:str|int) -> str:
     """ No safety built-in - will throw! """
     right = string.split(f'{char1}')[1]
     return right.split(f'{char2}')[0]
+
+def is_AlphaNumeric(string:str) -> str:
+    """ Works for languages other than English too! """
+    return string.isalnum()
