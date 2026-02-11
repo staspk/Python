27b2b26c1a9b73e3dddef7132908f8080075a8cf
Stanislav Kozubenko <staspk@gmail.com>
2026-02-11 00:25:06 -0800

    String.list() -> Str.From()

--- string.py
+++ string.py
@@ -1,14 +1,13 @@
+class Str:
+    def From(items:list, separator="\n\n") -> str:
+        """ Transforms list into a pretty string. """
+        string = ""
+        for item in items:
+            string += f'{item}{separator}'
 
-class String():
-    def list(_list:list, flip=False) -> str:
-        """ Transforms a list into a pretty string """
-        _str = ""
-        if(flip):
-            _list = reversed(_list)
-        for item in _list:
-            _str += f'{item}\n\n'
-        return _str[:-1]
+        return string[:-1]
 
+class String:
     def isEmptyOrWhitespace(string:str) -> bool:
         if not string or string.isspace():
             return True
