646981a4acdf9619a75b6da4958e417e66b884de
Stanislav Kozubenko <staspk@gmail.com>
2026-02-26 07:58:55 -0800

    +script.py

--- script.py
+++ script.py
@@ -4,30 +4,45 @@ from kozubenko.print import Print
 
 
 
-def Arg1(required=True) -> Any:
+def Arg1(value="", *, required=True) -> str:
     """
     **EXAMPLE:**
     ```python
     from kozubenko import script
     search:str = script.Arg1()
+    search:str = script.Arg1("Angel of the Lord")  # force value
+    search:str = script.Arg1(required=False) or "Angel of the Lord"  # Arg1 preferred, default value if missing
     ```
     """
-    if len(sys.argv) < 2:
-        Print.red('search.py: Missing: arg1 -> search:str')
+    arg1 = arg(1)
+
+    if value:
+        return value
+
+    if arg1.missing() and required is True:
+        Print.red('Arg1 Missing!')
         sys.exit(1)
-    return sys.argv[1]
 
-
-
-class Script:
-
-    @staticmethod
-    def Arg1(required=True) -> Any:
-        if len(sys.argv) < 2:
-            Print.red('search.py: Missing: arg1 -> search:str')
-            sys.exit(1)
+    if arg1.exists():
         return sys.argv[1]
+    
+    if required is False:
+        return ""
+    
+    raise Exception("Arg1(): Unexpected Runtime Path")
 
+
+class arg(int):
+    def __init__(self, index):
+        self.index = index
+
+    def exists(self):
+        if len(sys.argv) < self+1: return False
+        return True
+
+    def missing(self):
+        if len(sys.argv) < self+1: return True
+        return False
 
 
 
