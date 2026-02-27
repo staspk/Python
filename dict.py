eb8835864354d41d323555a6794ca95101cc74a1
Stanislav Kozubenko <staspk@gmail.com>
2026-02-26 20:22:03 -0800

    +Dict.key(), +Dict.value()

--- dict.py
+++ dict.py
@@ -1,4 +1,5 @@
 from typing import Any
+from itertools import islice
 
 
 def increment_value(dict:dict, key:Any):
@@ -9,3 +10,17 @@ class Dict:
     def print_items(dict:dict):
         for key,value in dict.items():
             print(f'{key}: {value}')
+
+    def key(dict:dict, index:int):
+        """
+        Pull key from dict by index without building a list.
+        O(n) traversal, use only when dicts are very long, many operations 
+        """
+        return next(islice(dict.keys(), index, index+1))
+
+    def value(dict:dict, index:int):
+        """
+        Pull value from dict by index without building a list.
+        O(n) traversal, use only when dicts are very long, many operations
+        """
+        return next(islice(dict.values(), index, index+1))
